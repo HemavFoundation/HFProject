cab96487cbc5a1d766eebfda6f7ce168e5a79977
Ruy Mallo <rmallo@hemav.com>
2020-04-02 19:15:36 +0200

    Initial commit with basic configuration

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+    <Test testProp="This is a property passed from the App.vue"/>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
