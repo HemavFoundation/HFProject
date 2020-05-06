f6fdaf795cb263df5684604e1f3692cfa7d02ef3
CarolaMoltoSuazo <camolsua@gmail.com>
2020-05-06 18:51:13 +0200

    creación json con datos falseados para crear la tabla

--- Downloads/HFProject-master/client/src/components/Tabla.vue
+++ Downloads/HFProject-master/client/src/components/Tabla.vue
@@ -1,3 +1,11 @@
 <template>
-<h1 class="display-2 font-weight-bold mb-3">CAROLA</h1>
+<v-data-table :headers="headers" :items="desserts" class="elevation-1"
+    >
 </template>
+<script>
+export default {
+    
+}
+</script>
+
+<h1 class="display-2 font-weight-bold mb-3">CAROLA</h1>
