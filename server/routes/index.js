218128f6e8819e571dcfc0042a84fa49142f8d8b
santiagodelhierro98 <santidelhierro98@gmail.com>
2020-05-10 12:31:45 +0200

    eliminado los ficheros de prueba product

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,11 +9,6 @@ const api = express.Router()
 
 
 //el auth me permite pedir autorizacion mediante un token (que va en la cabecera bearer) para poder hacer algunas acciones como post o delete. El get de product no necesito porque si que todo el mundo debe poder ver los productos
-// api.get('/product',productCtrl.getProducts)
-// api.get('/product/:productId', productCtrl.getProduct)
-// api.post('/product',auth, productCtrl.saveProduct)
-// api.put('/product/:productId',auth, productCtrl.updateProduct)
-// api.delete('/product/:productId',auth, productCtrl.deleteProduct)
 
 //rutas autenticación
 api.post('/signup',userCtrl.signUp)
