809949f55e7e71104cac0e50d60c6fb6de2f81f2
FP-UOC Cristian v <cviaplanaa@fp.uoc.edu>
2020-09-08 18:03:33 +0200

    Add dynamic tables from JSON
    Added dynamic tables reading data from JSON and separeting logic from structure through table components for each table view

--- client/src/views/DroneList.vue
+++ client/src/views/DroneList.vue
@@ -2,20 +2,35 @@
   <div>
     <Header />
     <Navbar />
-
+    <div class="my-2 mx-2">
+      <router-link to="RegisterDrone">
+      <v-btn depressed small color="primary" class="droneListMarginLeft">Create a drone</v-btn>
+      </router-link>
+      <v-btn depressed small color="primary" class="droneListMarginLeft">Upload drone data</v-btn>
+    </div>
+  <data-table tableEntry="dronelist"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue';
-import Header from '@/components/Header.vue';
+import Navbar from "@/components/Navbar.vue";
+import Header from "@/components/Header.vue";
+import axios from 'axios';
+import dataTable from '@/components/DroneListTable.vue'
 
 export default {
-  name: 'DroneList',
+  name: "DroneList",
   components: {
     Navbar,
-    Header
-  }
+    Header,
+    dataTable
+  },
+};
+</script>
+
+<style scoped>
+.droneListMarginLeft{
+margin-left: 20px;
 }
 </script>
