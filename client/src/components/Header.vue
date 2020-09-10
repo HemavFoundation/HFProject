9683fc9ec564556ecdf4c54742f0252b14dd74e7
cristianvia <cristianviar@gmail.com>
2020-09-10 14:14:33 +0200

    Merging 1/2

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -15,7 +15,7 @@
       <v-toolbar-title class="title">Drone Operations Management Application</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-toolbar-title>Hello username</v-toolbar-title>
+      <v-toolbar-title>Hello {{this.$store.state.user.userName}}</v-toolbar-title>
       <v-menu bottom left>
             <template v-slot:activator="{ on }">
               <v-btn
@@ -41,12 +41,15 @@
 
 <script>
 import { mapGetters } from 'vuex';
-
 export default {
   name: "Header",
   computed: {
     ...mapGetters({
-      userToken: 'user/getToken'
+      userToken: "user/getToken",
+      userEmail: "user/getEmail",
+      userName: "user/getUserName",
+      userSurName: "user/getSurName",
+      userCountry: "user/getCountry"
     })
   },
 };
@@ -62,4 +65,4 @@ color: white;
   left: 160px;
   color: white;
 }
-</style>>
+</style>
