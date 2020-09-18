f5fa93688674273dc0907ef597c54daa1d1173ee
Adria <adri.navarrete.09@gmail.com>
2020-09-18 15:50:45 +0200

    added info into rockblock.js

--- server/controllers/rockblock.js
+++ server/controllers/rockblock.js
@@ -1,4 +1,16 @@
 function recieveLocation(req, res) {
+    /*
+    The body of the JSON recived is:
+    {
+        'lat': lat,
+        'lon': lon,
+        'alt:': alt,
+        'heading': heading,
+        'typ': typ,  # type of the message for decoding
+        'status': status #status insde of the type of message for the correct decoding
+    }
+    */
+   
     console.log(req.body) // Call your action on the request here
     res.status(200).end() // Responding is important
 }
