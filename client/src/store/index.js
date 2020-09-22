5d4272af5886898ad422431513a8ee9d42562df6
sjove <sjove@hemav.com>
2020-09-22 16:22:50 +0200

    Using new module created as map

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import map from './modules/map'
 import VuexPersist from 'vuex-persist'
 
 
@@ -12,7 +13,8 @@ const vuexPersist = new VuexPersist({
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    map
   },
   plugins: [vuexPersist.plugin]
 })
