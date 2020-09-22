f2b4636c2bf5a5c0f6925d37b836e1381aa1d911
Adrià Navarrete <anavarrete@hemav.com>
2020-09-22 10:19:34 +0200

    JSON body modified

--- server/controllers/rockblock.js
+++ server/controllers/rockblock.js
@@ -1,18 +1,20 @@
 function recieveLocation(req, res) {
-    /*
-    The body of the JSON recived is:
-    {
-        'lat': lat,
-        'lon': lon,
-        'alt:': alt,
-        'heading': heading,
-        'typ': typ,  # type of the message for decoding
-        'status': status #status insde of the type of message for the correct decoding
-    }
-    */
-   
-    console.log(req.body) // Call your action on the request here
-    res.status(200).end() // Responding is important
+  /*
+  The body of the JSON recived is:
+  {
+      {
+        "drone_id": "HP2-087",
+        "lat": 41.2752192,
+        "lon": 1.9858253,
+        "alt:": 0.733,
+        "heading": 21,
+        "typ": 1,  # type of the message for decoding
+        "status": 01 #status insde of the type of message for the correct decoding
+  }
+  */
+
+  console.log(req.body) // Call your action on the request here
+  res.status(200).end() // Responding is important
 }
 
 module.exports = {
