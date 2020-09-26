f79c0ab6646c7f963c6726579360b6db0234ad57
SagredoIsmael <sagredo.ismael@live.com>
2020-09-27 01:36:55 +0200

    remove read file

--- server/controllers/flight.js
+++ server/controllers/flight.js
@@ -7,11 +7,11 @@ const mongoose = require('mongoose')
 const { db } = require('../models/drone')
 const { stringify } = require('querystring')
 
-function getFlightDetails (req,res) {
-    Flight.find({}, (err,flight) =>{
-        if (err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
-        if (!flight) return res.status(404).send({message: 'No existen vuelos'})
-    
+function getFlightDetails(req, res) {
+    Flight.find({}, (err, flight) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
+        if (!flight) return res.status(404).send({ message: 'No existen vuelos' })
+
         res.status(200).send(flight)
     })
 }
@@ -22,39 +22,29 @@ function getFlightDetails (req,res) {
 
 //store result.json in mongo db. bool function that once recognized user id, then checks flight id one by one and stores all data in the database
 
-function storeFlights(req,res) {
+function storeFlights(req, res) {
 
     let flights_data = req.body;
-
-    //results.json only for testing. Here we will have to put the usb path that the user will use.
-    const fs = require('fs');
-
-    //see fs.readfilesync because could be better when using directory instead of results.json
-    fs.readFile(flights_data, (err, data) => {
-
-        if (err) return res.status(500).send({ message: `Error al leer el json de prueba: ${err}` });
-
-        //convert to JSON format 
-        var flight = JSON.parse(data)
-
-
-        flight.dataOfFlights.forEach(async flightData => {
-            var idplate = flightData.id_plate;
-            var original_id = mongoose.Types.ObjectId();
-            var result = await db.collection('drones').findOne({ IdPlate: idplate });
-            original_id = result._id;
+    
+    flights_data.dataOfFlights.forEach(async flightData => {
+        var idplate = flightData.id_plate;
+        var original_id = mongoose.Types.ObjectId();
+        var result = await db.collection('drones').findOne({ IdPlate: idplate });
+        original_id = result._id;
+        try {
             db.collection('flights').insertOne(
                 {
                     "dronedbID": original_id,
                     ...flightData
                 },
                 { upsert: true }
-            )
-            res.status(200).send({message:'Resultados del JSON leidos y guardados correctamente'})
-        });
-
-    });
-
+            ),
+            res.status(200).send({ message: 'Resultados del JSON leidos y guardados correctamente' })
+        } catch (e) {
+            print(e);
+            res.status(500).send({ message: e })
+        }
+    })
 }
 
 
