fe82d1e7676dc8fb1dfce6b8a9b7c4c781bb2466
Laura Pujol <laura.pujol98@gmail.com>
2020-09-30 17:02:02 +0200

    checkpoint for gitlab

--- server/controllers/rockblock.js
+++ server/controllers/rockblock.js
@@ -1,72 +1,74 @@
 const fs = require('fs');
 const hex = require('hex-encode-decode');
 const Location = require('../models/location')
-const { db } = require('../models/drone')
+const { db } = require('../models/drone');
+const { type } = require('os');
 
-function recieveLocation(req, res) {
+function data_interpreter(string) {
+  ljhewfvneñwfvevev
+  let obj = {
+    id_plate: string,
+    type: 1
+
+  }
+  return obj
+}
+
+const rawdata = req.body
+
+const JSON = data_interpreter(hex.decode(rawdata.data))
+
+
+
+function recieveMessage(req, res) {
   /*
   The body of the JSON recived is:
   {
     "drone_id": "HP2-087",
+    "status": 0, #status for the correct decoding (landing/flight)
     "lat": 41.2752192,
     "lon": 1.9858253,
     "alt:": 0.733,
     "heading": 21,
-    "typ": 1,  # type of the message for decoding
-    "status": 01 #status insde of the type of message for the correct decoding
+    "mission": periscope,  # type of flight
   }
   */
-  
+
   const rawdata = req.body
-  
-  const jsondata = JSON.parse(hex.decode(rawdata.data))
-  
+
+  const jsondata = data_interpreter(hex.decode(rawdata.data))
+
   const time = rawdata.transmit_time
   const id_plate = jsondata.drone_id
+  const status = jsondata.type
   const lat = jsondata.lat
   const lon = jsondata.lon
   const alt = jsondata.alt
   const heading = jsondata.heading
+  const mission = jsondata.mission
   
-  const location = {
-    id_plate: id_plate,
-    time: time,
-    lat: lat,
-    lon: lon,
-    alt: alt,
-    heading: heading
-  }
-  db.collection('Location').insertOne( 
-    location,
-    { upsert: true }
-    ).then(() => {
-      res.status(200).send({ ok: true })
-    }).catch((error) => {
-      res.send({ ok: false, error })
-    })
-    
-  }
-  
-  const getFlightsLocations = (req, res) => {
-    db.collection('Location').distinct('id_plate').then((idPlates= []) => {
-      const promises = idPlates.map(idPlate => db.collection('Location')
-        .find({ id_plate: { $eq: idPlate } })
-        .sort({_id: -1})
-        .limit(1)
-        .toArray()
-        .then((droneLocation = []) =>  droneLocation.length  && droneLocation[0])
-      )
+}
 
-      Promise.all(promises)
-        .then(allLocations => {
-          res.send(allLocations.filter(Boolean))
-        }).catch(error => {
-          res.status(500).send({ok: false, error})
-        })
-    })
-  }
-  
-  module.exports = {
-    recieveLocation,
-    getFlightsLocations
-  };
+const getFlightsLocations = (req, res) => {
+  db.collection('Location').distinct('id_plate').then((idPlates = []) => {
+    const promises = idPlates.map(idPlate => db.collection('Location')
+      .find({ id_plate: { $eq: idPlate } })
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray()
+      .then((droneLocation = []) => droneLocation.length && droneLocation[0])
+    )
+
+    Promise.all(promises)
+      .then(allLocations => {
+        res.send(allLocations.filter(Boolean))
+      }).catch(error => {
+        res.status(500).send({ ok: false, error })
+      })
+  })
+}
+
+module.exports = {
+  recieveMessage,
+  getFlightsLocations
+};
--- server/models/location.js
+++ server/models/location.js
@@ -5,10 +5,12 @@ const Schema = mongoose.Schema
 const LocationSchema = Schema ({
     id_plate: String,
     time: String,
+    status: { type:Number, default: null},
     lat: { type:Number, default: null},
     lon: { type:Number, default: null},
     alt: { type:Number, default: null},
-    heading: { type:Number, default: null}
+    heading: { type:Number, default: null},
+    mission: { type:String, default: null},
 })
 
 module.exports = mongoose.model('Location', LocationSchema)
